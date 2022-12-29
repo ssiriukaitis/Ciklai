0d54ad8e5be09fd74b3a43070c40c1efa8a8e155
ssiriukaitis <sigitas.siriukaitis@gmail.com>
2022-12-29 10:12:51 +0200

    1 uzduotis

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,54 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       // System.out.println("Hello world!");
+
+        //==============1 uzd =======================
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + " Labas");
+        }
+
+
+        //================== 2 uzd ===================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 }
